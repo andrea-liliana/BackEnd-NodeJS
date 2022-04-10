9d2c2808bb45d3800ecd7022ee09e642677a8ac6
Andrea Gómez <andrea.mx1998@gmail.com>
2022-04-10 16:49:45 +0200

    Upload examples

---
+++ weekly_mission_1/example8/logger.js
@@ -0,0 +1,17 @@
+// EcmaScript Export Default
+
+/*
+  export default nos permite exportar esta clase e importara
+
+  import MyLogger from './logger.js'
+*/
+
+export default class Logger {
+    constructor (name) {
+      this.name = name
+    }
+  
+    log (message) {
+      console.log(`[${this.name}] ${message}`)
+    }
+}
---
+++ weekly_mission_1/example8/main_2.js
@@ -0,0 +1,6 @@
+// EcmaScript Export Default
+
+/* Importando el módulo */
+import * as loggerModule from './logger.js'
+
+console.log(loggerModule)
