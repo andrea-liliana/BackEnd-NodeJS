6959f6c6c76006c19812e8d6d2eb8480150af2e3
andrea-liliana <andrea.mx1998@gmail.com>
2022-04-22 14:46:56 +0200

    Agregando ejemplo de creacion de objeto con propiedades

--- weekly_mission_2/examples_0.js
+++ weekly_mission_2/examples_0.js
@@ -4,3 +4,11 @@ console.log("Objetos")
 const myObjetc = {} // Esto es un objeto vacío
 console.log("Ejemplo 1: Crear un objeto vacío")
 console.log(myObjetc)
+
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Carlo",
+    age: 27
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObjetc2)
